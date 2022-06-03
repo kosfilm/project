ec72ca474019b9319ff65318fe3c556047b24fc3
Kostiantyn <kosfilmproduct@gmail.com>
2022-06-03 15:03:28 +0300

    'movis_app'

--- script.js
+++ script.js
@@ -59,3 +59,32 @@
 // console.log(isChecked || !isClose);
 
 
+
+
+let numberOffilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+
+const pesonalMovieDB = {
+	count: numberOffilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', '');
+// c = prompt('Один из последних просмотренных фильмов?', ''),
+// d = prompt('На сколько оцените его?', '');
+
+
+pesonalMovieDB.movies[a] = b;
+// pesonalMovieDB.movies[c] = d;
+
+
+
+
+
+console.log(pesonalMovieDB)
