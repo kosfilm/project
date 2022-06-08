9da40b601aee7aa646177ba89ede1cedd0a6f6ab
Kostiantyn <kosfilmproduct@gmail.com>
2022-06-08 17:34:25 +0300

    function

--- script.js
+++ script.js
@@ -61,9 +61,278 @@
 
 
 
+// let numberOffilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const pesonalMovieDB = {
+// 	count: numberOffilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// };
+
+// let a = prompt('Один из последних просмотренных фильмов?', ''),
+// 	b = prompt('На сколько оцените его?', '');
+
+// pesonalMovieDB.movies[a] = b;
+// // pesonalMovieDB.movies[c] = d;
+
+
+// console.log(pesonalMovieDB)
+
+
+
+
+// if (4 == 9) {
+// 	console.log('work');
+// }
+// else {
+// 	console.log('not work');
+// }
+
+// let num = 56;
+
+// switch (num) {
+// 	case 55:
+// 		console.log('55')
+// 		break;
+// 	case 40:
+// 		console.log('55')
+// 		break;
+// 	default:
+// 		console.log('not')
+// 		break;
+
+// }
+
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+// 	console.log('I have food');
+// }
+// else {
+// 	console.log('I dont have food');
+// }
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'gfbfgngf');
+
+
+// ========================= 015 (д) Логические операторы
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && (cola === 2 || fries === 3) && nuggets) {
+// 	console.log('I have food');
+// }
+// else {
+// 	console.log('I dont have food');
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+// let johnReport, alexReport, semReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || semReport || mariaReport);
+
+
+// console.log(!0);
+
+
+
+//================================== 016 Циклы
+
+// let num = 50;
+
+// while (num <= 55) {
+// 	console.log(num)
+// 	num++
+// }
+
+// do {
+// 	console.log(num)
+// 	num++
+// }
+// while (num <= 55)
+
+// for (let i = 1; i < 10; i++) {
+// 	if (i == 6) {
+// 		// break;
+// 		continue;
+// 	}
+// 	console.log(i)
+// }
+
+
+
+// for (let i = 0; i < 3; i++) {
+// 	console.log(i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(j);
+// 	}
+// }
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+// 	for (let j = 0; j < i; j++) {
+// 		result += '*';
+// 	}
+
+// 	result += '\n';
+// }
+
+// console.log(result);
+
+
+
+// first: for (let i = 0; i < 3; i++) {
+// 	console.log(`First level ${i}`);
+
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`First level ${j}`);
+
+// 		for (let k = 0; k < 5; k++) {
+
+// 			if (k === 2) break first;
+// 			console.log(`Third level ${k}`);
+
+// 		}
+// 	}
+// }
+
+// console.log(result);
+
+
+
+
+// ДЗ урок ======= 018 script.js
+
+
+// let numberOffilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// let personalMovieDB = {
+// 	count: numberOffilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// }
+// let i = 0;
+// do {
+// 	i++
+// 	let a = prompt('Один из последних просмотренных фильмов?', ''),
+// 		b = prompt('На сколько оцените его?', '');
+
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+// 		personalMovieDB[a] = b;
+// 		console.log('Done');
+// 	}
+// 	else {
+// 		console.log('Error');
+// 		i--;
+// 	}
+// }
+// while (i < 2)
+
+
+// if (personalMovieDB.count < 10) {
+// 	console.log("Просмотрено довольно мало фильмов")
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+// 	console.log("Вы классический зритель")
+// } else if (personalMovieDB.count >= 30) {
+// 	console.log("Вы киноман")
+// } else {
+// 	console.log("Произошла ошибка")
+// }
+
+
+// console.log(personalMovieDB)
+
+
+
+
+// ===================   019 Функции, стрелочные ф-ции (ES6)
+
+// let num = 20;
+
+// function showFirstMassage(text) {
+// 	console.log(text);
+// 	num = 10;
+// }
+
+// showFirstMassage('Hello Svit');
+
+// console.log(num);
+
+
+// function calc(a, b) {
+// 	return a + b;
+// }
+// console.log(calc(5, 13));
+// console.log(calc(5, 3));
+// console.log(calc(15, 13));
+
+// function ret() {
+// 	let num = 50;
+// 	return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function () {
+// 	console.log('Slava Ukraine');
+// };
+
+// logger();
+
+// const calc = (a, b) => a + b;
+
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(usd, amount) {
+// 	return usd * amount;
+// }
+
+// function promotion(result) {
+// 	console.log(result * discount);
+// 	return function () { }
+// }
+// const res = convert(usdCurr, 500);
+// promotion(res);
+
+// function test() {
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log(i);
+// 		if (i == 3) return;
+// 	}
+// 	console.log('Done');
+// }
+
+// test();
+
+// function doNoting() { }
+// console.log(doNoting() === undefined);
+
+
+
 let numberOffilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
-
 
 const pesonalMovieDB = {
 	count: numberOffilms,
@@ -73,18 +342,26 @@ const pesonalMovieDB = {
 	privat: false,
 };
 
+// let a = prompt('Один из последних просмотренных фильмов?', ''),
+// 	b = prompt('На сколько оцените его?', '');
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', '');
-// c = prompt('Один из последних просмотренных фильмов?', ''),
-// d = prompt('На сколько оцените его?', '');
-
-
-pesonalMovieDB.movies[a] = b;
-// pesonalMovieDB.movies[c] = d;
+// pesonalMovieDB.movies[a] = b;
 
 
+function showMyDB(hidden) {
+	if (!hidden.privat) console.log(hidden);
+}
+
+showMyDB(pesonalMovieDB);
+
+function writeYourGenres(gen) {
+	for (let i = 1; i <= 3; i++) {
+		gen[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
+
+writeYourGenres(pesonalMovieDB.genres);
+
+// ========================== 022 Методы и свойства строк и чисел
 
 
-
-console.log(pesonalMovieDB)
